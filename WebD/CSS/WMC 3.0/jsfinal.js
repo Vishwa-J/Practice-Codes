9e7ffcd01fc46ef77a17b7944da7a007f1fb21c1
Vishwa-J <107062706+Vishwa-J@users.noreply.github.com>
2023-01-03 10:20:23 +0530

    Add files via upload

---
+++ WebD/CSS/WMC 3.0/jsfinal.js
@@ -0,0 +1,42 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+let searchForm = document.getElementByIdquerySelector('signup');
+
+document.getElementById('signup').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
